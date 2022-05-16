781177c42e39604668847bab2ae38a2c478b3255
ntk <43851479+ntk-ta01@users.noreply.github.com>
2022-05-16 10:43:31 +0900

    1.5s,dfs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ type Output = String;
 const DIJ: [(usize, usize); 4] = [(!0, 0), (1, 0), (0, !0), (0, 1)];
 const DIR: [char; 4] = ['U', 'D', 'L', 'R'];
 
+const TIMELIMIT: f64 = 1.5;
+
 struct Input {
     s: (usize, usize),
     tiles: Vec<Vec<usize>>,
@@ -22,9 +24,11 @@ fn parse_input() -> Input {
 }
 
 fn main() {
+    let mut timer = Timer::new();
     let input = parse_input();
     let out = {
-        let mut out: Vec<char> = vec![];
+        let mut best_out = vec![];
+        let mut out = vec![];
         let m = *input
             .tiles
             .iter()
@@ -34,13 +38,27 @@ fn main() {
             + 1;
         let mut used_tiles = vec![false; m];
         used_tiles[input.tiles[input.s.0][input.s.1]] = true;
-        dfs(&input, &mut out, input.s, &mut used_tiles);
-        out.iter().collect::<Output>()
+        dfs(
+            &input,
+            &mut out,
+            input.s,
+            &mut used_tiles,
+            &mut timer,
+            &mut best_out,
+        );
+        best_out.iter().collect::<Output>()
     };
     println!("{}", out);
 }
 
-fn dfs(input: &Input, out: &mut Vec<char>, v: (usize, usize), used_tiles: &mut Vec<bool>) {
+fn dfs(
+    input: &Input,
+    out: &mut Vec<char>,
+    v: (usize, usize),
+    used_tiles: &mut Vec<bool>,
+    timer: &mut Timer,
+    best_out: &mut Vec<char>,
+) {
     for (i, (di, dj)) in DIJ.iter().enumerate() {
         let ni = v.0 + di;
         let nj = v.1 + dj;
@@ -53,7 +71,41 @@ fn dfs(input: &Input, out: &mut Vec<char>, v: (usize, usize), used_tiles: &mut V
         }
         out.push(DIR[i]);
         used_tiles[input.tiles[ni][nj]] = true;
-        dfs(input, out, (ni, nj), used_tiles);
-        return;
+        dfs(input, out, (ni, nj), used_tiles, timer, best_out);
+        if best_out.len() < out.len() {
+            *best_out = out.clone();
+        }
+        if TIMELIMIT < timer.get_time() {
+            return;
+        }
+        out.pop();
+        used_tiles[input.tiles[ni][nj]] = false;
     }
+}
+
+fn get_time() -> f64 {
+    let t = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap();
+    t.as_secs() as f64 + t.subsec_nanos() as f64 * 1e-9
+}
+
+struct Timer {
+    start_time: f64,
+}
+
+impl Timer {
+    fn new() -> Timer {
+        Timer {
+            start_time: get_time(),
+        }
+    }
+
+    fn get_time(&self) -> f64 {
+        get_time() - self.start_time
+    }
+
+    // fn reset(&mut self) {
+    //     self.start_time = get_time();
+    // }
 }
